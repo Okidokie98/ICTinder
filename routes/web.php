26a517a748076c985c86930c2b768cf8ea248f18
Jeroen Rijken <rijk0085@hz.nl>
2018-03-20 22:16:36 +0100

    eerste 3 html pagina's opgezet

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/overview', function () {
+    return view('overview');
+});
+
+Route::get('/chat', function () {
+    return view('chat');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
