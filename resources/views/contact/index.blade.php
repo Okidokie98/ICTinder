93aec23398662d5325acf06fc2b5e83db62985eb
baat0004 <baat0004@hz.nl>
2018-03-21 15:57:58 +0100

    Setting up the contact page

---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/bootstrap/dist/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+    <title>Contact the company</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
